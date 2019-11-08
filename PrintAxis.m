04069da923bcc961a6ff4bcabfe5ab42f7420fcd
dulce-torres <54420626+dulce-torres@users.noreply.github.com>
2019-11-07 20:54:01 -0600

    Add files via upload

---
+++ PrintAxis.m
@@ -0,0 +1,5 @@
+function PrintAxis ()
+line([-9 9],[0 0],[0 0],'color',[1 0 0],'linewidth',2.5); %establece el eje X en rojo
+ line([0 0],[-9 9],[0 0],'color',[0 1 0],'linewidth',2.5); %establece el eje Y en verde
+  line([0 0],[0 0],[-9 9],'color',[0 0 1],'linewidth',2.5); %establece el eje Y en verde
+end
